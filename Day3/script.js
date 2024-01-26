ef258f621a5e95431fbe8ec8a93fbc31864f4f3d
Miskat <misky0310@gmail.com>
2024-01-26 19:35:44 +0530

    DAY3- UPDATING VARIABLES IN CSS- Completed

---
+++ Day3/script.js
@@ -0,0 +1,13 @@
+const inputs=document.querySelectorAll(".controls input")
+
+const handleUpdate = (e) => {
+    const suffix=e.target.dataset.sizing || '';
+    const value=(e.target.value);
+    const targetName=(e.target.name);
+    console.log(value+suffix);
+    document.documentElement.style.setProperty(`--${targetName}`,value+suffix)
+}
+
+inputs.forEach(input => {
+    input.addEventListener('change', handleUpdate)
+});
